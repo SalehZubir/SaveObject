27fb5f589e8e05df14b9cdbac6acb253848b3e9e
Saleh <salehzubir@gmail.com>
2022-02-15 16:54:13 +0300

    Initial Commit

---
+++ SaveDataAsObjects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaveDataAsObjects
+//
+//  Created by Saleh Omar Ahmed on 15/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
